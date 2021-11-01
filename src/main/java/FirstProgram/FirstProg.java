ff3935df1bf38d3b395dcca0d83d89874614ea14
PrabhatT1989 <Prabhat.Tavag@gmail.com>
2021-11-01 11:50:06 +0530

    first commit

---
+++ src/main/java/FirstProgram/FirstProg.java
@@ -0,0 +1,13 @@
+package FirstProgram;
+
+public class FirstProg {
+
+	public static void main(String[] args) {
+	System.out.println("Hello world");
+	
+	
+	System.out.println("Hello Maven");
+
+	}
+
+}
---
+++ src/main/java/FirstProgram/SecondProg.java
@@ -0,0 +1,12 @@
+package FirstProgram;
+
+public class SecondProg {
+
+	public static void main(String[] args) {
+		System.out.println("Second Maven Program");
+
+		
+		System.out.println("We will practice the local programs oncewe get time");
+	}
+
+}
