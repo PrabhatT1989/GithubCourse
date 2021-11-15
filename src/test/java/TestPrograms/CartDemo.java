a9c1107d69de9045a0a2f1992a225275fffa758c
PrabhatT1989 <Prabhat.Tavag@gmail.com>
2021-11-15 15:24:14 +0530

    Remove elements from cart

--- src/test/java/TestPrograms/CartDemo.java
+++ src/test/java/TestPrograms/CartDemo.java
@@ -3,13 +3,13 @@ package TestPrograms;
 public class CartDemo {
 	
 	public static void main(String args[])
-	{	
-			
+	{				
 System.out.println("This is CartDemo ");
 
 System.out.println("Add to Cart Functionality");
 
-	}
+System.out.println("Remove Products from cart");
+}
 }
 
 
