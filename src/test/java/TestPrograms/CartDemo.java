1bc451b3b789558eed23b5ed8b3829ce4104237c
PrabhatT1989 <Prabhat.Tavag@gmail.com>
2021-11-17 01:05:24 +0530

    cart modified

--- src/test/java/TestPrograms/CartDemo.java
+++ src/test/java/TestPrograms/CartDemo.java
@@ -9,6 +9,11 @@ System.out.println("This is CartDemo ");
 System.out.println("Add to Cart Functionality");
 
 System.out.println("Remove Products from cart");
+
+System.out.println("Again add Products from cart");
+
+System.out.println("Add single product at a time");
+
 }
 }
 
